c8c5116a55cd481ec52cbf00bd963418da4ebecb
Victor Elias Barrera Florez <vbarrera@outlook.com>
2023-06-17 14:01:26 -0500

    refactor: Mejoras para recibir mas de 2 radares.

--- src/main/java/banckend/SerialModbusExample.java
+++ src/main/java/banckend/SerialModbusExample.java
@@ -1,40 +1,65 @@
 package banckend;
 
+
 import com.ghgande.j2mod.modbus.facade.ModbusSerialMaster;
 import com.ghgande.j2mod.modbus.procimg.Register;
 import com.ghgande.j2mod.modbus.util.SerialParameters;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class SerialModbusExample {
+    Dotenv dotenv;
+
+    public SerialModbusExample(Dotenv dotenv) {
+        this.dotenv = dotenv;
+    }
+
     public static void main(String[] args) {
-        try {
 
-            SerialParameters params = new SerialParameters();
-            params.setPortName("/dev/cu.usbserial-14620");
-            params.setBaudRate(4800);
-            params.setDatabits(8);
-            params.setParity("none");
-            params.setStopbits(1);
+        SerialModbusExample example = new SerialModbusExample(Dotenv.load());
+        example.modbusSerialMaster();
 
-            ModbusSerialMaster modbusMaster = new ModbusSerialMaster(params);
-            modbusMaster.connect();
+    }
 
-            int slaveId = 2;
-            int startAddress = 0;
-            int numInputs = 10;
-            for (int ix = 0; ix < 50; ix++) {
+    private SerialParameters serialParameters() {
+        SerialParameters params = new SerialParameters();
+        params.setPortName(dotenv.get("PORT_NAME"));
+        params.setBaudRate(4800);
+        params.setDatabits(8);
+        params.setParity("none");
+        params.setStopbits(1);
+        return params;
+    }
 
-                Register[] datos = modbusMaster.readMultipleRegisters(slaveId, startAddress, numInputs);
-                Thread.sleep(5000);
+    private void modbusSerialMaster() {
 
-                for (int i = 0; i <datos.length; i++) {
-                    System.out.print(" "+datos[i].toString());
+
+        String[] slaveIDS = dotenv.get("SLAVE_IDS").split(",");
+        ModbusSerialMaster modbusMaster = new ModbusSerialMaster(serialParameters());
+
+        for (String slave : slaveIDS) {
+            try {
+
+                modbusMaster.connect();
+
+                int slaveId = Integer.parseInt(slave);
+                int startAddress = 0;
+                int numInputs = 10;
+                for (int ix = 0; ix < 50; ix++) {
+
+                    Register[] datos = modbusMaster.readMultipleRegisters(slaveId, startAddress, numInputs);
+                    Thread.sleep(5000);
+
+                    for (Register data : datos) {
+                        System.out.print(" " + data.toString());
+                    }
+                    System.out.println();
                 }
-                System.out.println();
+
+                modbusMaster.disconnect(); // Desconectar del puerto serial
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
 
-            modbusMaster.disconnect(); // Desconectar del puerto serial
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
